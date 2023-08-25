be17748dbe824dd4e45192d4aa203fe41a9c2e4d
Mohammad Kiaei <78914731+mohammadkiaei@users.noreply.github.com>
2023-08-25 12:04:28 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-//Select DOM Items
+// Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-//set Intial State Of Menu
+// set Intial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -18,7 +18,7 @@ function toggleMenu() {
     menuBranding.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));
 
-    //Set Menu State
+    // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove('close');
@@ -27,7 +27,9 @@ function toggleMenu() {
     menuNav.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
 
-    //Set Menu State
+    // Set Menu State
     showMenu = false;
   }
 }
+
+// Developed By Mohammad Kiaei
